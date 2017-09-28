b40777847bb116c209b32ee586c736b280774b6a
Omair <omairraza9@gmail.com>
2017-09-27 22:12:02 -0400

    second try

--- shoppingcart.rb
+++ shoppingcart.rb
@@ -6,9 +6,10 @@ class ShoppingCart
   end
 
 
-  def add_product(name, price, tax_rate)
-    add_product = ShoppingCart.new(name, price, tax_rate)
-    @products << add_product
+  def self.add_product(name, price, tax_rate)
+    @item = Products2buy.new(name, price, tax_rate)
+    @products << @item
+    return @item
   end
 
   def remove(name)
@@ -19,14 +20,24 @@ class ShoppingCart
   end
   ###stopped working here
 
-  def pretax
-  @pretax = pretax
+  def pretax(price)
+    sum = 0
+      @products.each do |product|
+        sum += product.price
+      end
   end
-
   def posttax
-    @posttax = posttax
+    sum = 0
+    @products.each do |product|
+      sum += product.price*product.tax_rate
+    end
   end
+  end
+end
   #add an product to the cart
 #remove an product from the cart
 #add up the total cost of all products in the cart before tax
 #add up the total cost of all products in the cart after tax
+
+product1 = ShoppingCart.add_product("oatmeal", 2.99, 0.13)
+puts "#{product1}"
